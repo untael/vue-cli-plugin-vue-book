07f8cc68e8ab377e2666958e9241169f63de796b
untael <pashamironow@gmail.com>
2020-05-04 00:10:08 +0300

    init: plugin

--- generator/index.js
+++ generator/index.js
@@ -7,5 +7,27 @@ module.exports = (api, opts) => {
     "vue-book": '^0.1.0-alpha.31',
   }
 
+  api.registerCommand(
+    'serve:book',
+    {
+      description: 'serve',
+      usage: 'serve:book'
+    },
+    () => {
+      console.log('serve:book')
+    }
+
+  )
+  api.registerCommand(
+    'build:book',
+    {
+      description: 'build',
+      usage: 'build:book'
+    },
+    () => {
+      console.log('build:book')
+    }
+  )
+
   api.extendPackage(dependencies)
 }
